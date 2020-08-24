63e77fd8cbd666dc1709b8408a1f8e64d9031239
ruhci28 <59759735+ruhci28@users.noreply.github.com>
2020-08-24 09:08:32 +0530

    Updates

---
+++ precache-manifest.ff5aa8fd986afce8d7a0498139cae8ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f989f04935185b8e976d30754e4e644b",
+    "url": "/basic-blog/index.html"
+  },
+  {
+    "revision": "dc3219455ad1d5248246",
+    "url": "/basic-blog/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbd6812f656f1eb98829",
+    "url": "/basic-blog/static/js/2.e806fae2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/basic-blog/static/js/2.e806fae2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3219455ad1d5248246",
+    "url": "/basic-blog/static/js/main.38f83da5.chunk.js"
+  },
+  {
+    "revision": "ae5ed882742ce72d8415",
+    "url": "/basic-blog/static/js/runtime-main.6afb980c.js"
+  }
+]);
